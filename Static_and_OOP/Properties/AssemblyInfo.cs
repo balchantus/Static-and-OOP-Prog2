fcc743248430e7f286b2cb7ca998da803ac27eed
balchantus <89977635+balchantus@users.noreply.github.com>
2021-09-29 18:55:07 +0200

    Add files via upload

---
+++ Static_and_OOP/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Klass med 2 Metoder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Klass med 2 Metoder")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a166b37b-2656-4427-9b65-131b59935d9f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Static_and_OOP/Räkna.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Static_and_OOP
+{
+    class Räkna
+    {
+        public static double tal1 = 0;
+        public static double tal2 = 0;
+        public static double svar = 0;
+
+        public void Answer()
+        {
+            Console.WriteLine("Svar: " + svar);
+        }
+
+        public void Multiplikation() 
+        {
+            svar = tal1 * tal2;
+
+            Answer();
+        }
+
+        public void Division()
+        {
+            svar = tal1 / tal2;
+
+            Answer();
+        }
+
+        public void Addition() 
+        {
+            svar = tal1 + tal2;
+
+            Answer();
+        }
+
+        public void Subtraction()
+        {
+            svar = tal1 - tal2;
+
+            Answer();
+        }
+    }
+}
