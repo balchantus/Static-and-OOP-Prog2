8c8eb8b47b0b63a02fab9e8b3fbe9a3a54d87b32
balchantus <89977635+balchantus@users.noreply.github.com>
2021-10-06 10:34:53 +0200

    Add files via upload

---
+++ Static_and_OOP/Input.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static Static_and_OOP.Räkna;
+
+namespace Static_and_OOP
+{
+    class Input
+    {
+        public void pInput()
+        {
+            Console.Write("Mata in första talet: ");
+            tal1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Mata in andra talet: ");
+            tal2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ Static_and_OOP/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Static_and_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Räkna Calculate = new Räkna();
+            Input inP = new Input();
+
+            while (true)
+            {
+                Console.Write("Multiplikation, Division, Addition eller Subtraction? (M, D, A eller S); ");
+                string sätt = Console.ReadLine();
+
+                inP.pInput();
+
+                if (sätt == "M" || sätt == "m")
+                {
+                    Calculate.Multiplikation();
+                }
+
+                else if (sätt == "D" || sätt == "d")
+                {
+                    Calculate.Division();
+                }
+
+                else if (sätt == "A" || sätt == "a")
+                {
+                    Calculate.Addition();
+                }
+
+                else if (sätt == "S" || sätt == "s")
+                {
+                    Calculate.Subtraction();
+                }
+
+                else
+                {
+                    break;
+                }
+
+                Console.WriteLine("Svar: " + Calculate.svar);
+
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
